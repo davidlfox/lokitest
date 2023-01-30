48819a3aabf27fde2a1a0513471dadd16ec59f4a
fox <me@iamdavidfox.com>
2023-01-29 20:00:05 -0500

    first poc

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+
+internal class Program
+{
+    private const string app = "lokipoc-service";
+
+    private static void Main(string[] args)
+    {
+        var logger = LogManager.GetCurrentClassLogger();
+        logger.Info("inside lokipoc");
+
+        logger.Trace("trace message", new { app });
+        logger.Debug("debug message", new { app });
+        logger.Warn("warn message", new { app });
+        logger.Error("error message", new { app });
+        logger.Fatal("fatal message", new { app });
+
+        Console.WriteLine("done");
+    }
+}
